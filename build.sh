3c7900aaee694d95df025a0a60bf3d2edc0afbf6
YAMAN ALPARSLAN <y.alparslan@tescom-ups.com>
2026-01-23 08:51:57 +0300

    Fix: Remove migrate from build script

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# Hata olursa dur
+# exit on error
 set -o errexit
 
-# Kütüphaneleri yükle
 pip install -r requirements.txt
-
-# Veritabanı tablolarını oluştur
-python manage.py migrate
-
-# Eski statik dosyaları temizle ve yenilerini topla (Jazzmin kalıntılarını siler)
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
